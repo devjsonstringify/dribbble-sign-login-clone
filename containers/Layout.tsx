7bd0960e75c414f16e1b5c05c78b9d8904108dfc
devjson <dev.json.stringify@gmail.com>
2021-10-27 19:36:41 +0800

    Update: set up user interface left sidebar

--- containers/Layout.tsx
+++ containers/Layout.tsx
@@ -1,29 +1,88 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
+import Link from '@mui/material/Link'
 import Paper from '@mui/material/Paper'
 import { styled, useTheme } from '@mui/material/styles'
 import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material'
+import Logo from '../components/Logo'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   const theme = useTheme()
+
   return (
     <Box sx={{ height: '100%' }}>
       <Grid container sx={{ height: '100%' }}>
         <Grid
           item
           md={3}
-          sx={{ display: { xs: 'none', md: 'block' }, height: '100%' }}
+          sx={{ display: { sm: 'none', md: 'block' }, height: '100%' }}
         >
           <Box
             sx={{
-              bgcolor: theme.palette.primary.main,
+              bgcolor: theme?.dribbleAccents?.pink,
+              display: 'grid',
+              gridTemplateRows: '1fr 2fr',
+              justifyContent: 'space-between',
               height: '100%',
             }}
           >
-            left sidebar
+            <Box sx={{ padding: '4rem 4rem 0' }}>
+              <Box
+                sx={{
+                  height: 'auto',
+                  minWidth: '3.75rem',
+                  maxWidth: '5.625rem',
+                  marginBottom: '1.875rem',
+                }}
+              >
+                <Link href="https://dribbble.com/">
+                  <Logo
+                    ariaLabel="dribble-logo"
+                    fill={theme?.dribbleAccents?.brown}
+                    role="image"
+                  />
+                </Link>
+              </Box>
+              <Typography
+                variant="h4"
+                sx={{
+                  fontWeight: 700,
+                  color: theme?.dribbleAccents?.brown,
+                }}
+              >
+                Discover the world’s top Designers & Creatives.
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                flexGrow: 1,
+                height: '100%',
+                paddingBottom: '2rem',
+              }}
+            >
+              <LeftBgImage />
+              <Typography
+                sx={{
+                  paddingLeft: '4rem',
+                  color: theme?.dribbleAccents?.brown,
+                }}
+              >
+                Art by{' '}
+                <Link
+                  href="https://dribbble.com/karicca"
+                  sx={{ color: theme?.dribbleAccents?.brown }}
+                >
+                  {' '}
+                  Irina Valeeva
+                </Link>
+              </Typography>
+            </Box>
           </Box>
         </Grid>
         <Grid item xs={12} md={9} sx={{ height: '100%' }}>
@@ -42,3 +101,16 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
 }
 
 export default Layout
+
+/**
+ * @todo
+ * 1. set color via global theme, incorrect text color
+ */
+
+const LeftBgImage = styled(Box)({
+  backgroundImage: `url(/assets/procrastination.jpg)`,
+  flexGrow: 1,
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'contain',
+})
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,6 +1,22 @@
 import { createTheme } from '@mui/material/styles'
 import { red } from '@mui/material/colors'
 
+declare module '@mui/material/styles' {
+  interface Theme {
+    dribbleAccents: {
+      brown: string
+      pink: string
+    }
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    dribbleAccents?: {
+      brown?: string
+      pink?: string
+    }
+  }
+}
+
 // Create a theme instance.
 const theme = createTheme({
   palette: {
@@ -13,6 +29,10 @@ const theme = createTheme({
     error: {
       main: red.A400,
     },
+  },
+  dribbleAccents: {
+    brown: '#865c6c',
+    pink: '#F1CDD7',
   },
 })
 
