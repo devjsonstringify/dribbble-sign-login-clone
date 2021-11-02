d12773909f32ae86c514220e80a8d6fd5fb04209
devjson <dev.json.stringify@gmail.com>
2021-11-02 14:48:51 +0800

    Add: Forms component Fix: visibility logic

--- containers/RightSidebar/RightSidebarContent/RightSidebarContent.tsx
+++ containers/RightSidebar/RightSidebarContent/RightSidebarContent.tsx
@@ -4,15 +4,17 @@ import Divider from '@mui/material/Divider'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import SignedInToDribbble from '../../../components/SignedInToDribbble'
+import Forms from '../../../components/Forms'
 
 const RightSidebarContent = () => {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: 'flex', height: '100%' }}>
       <Container maxWidth="sm" sx={{ margin: 'auto' }}>
         <SignedInToDribbble />
         <Box sx={{ my: '2rem' }}>
           <Divider>Or</Divider>
         </Box>
+        <Forms />
       </Container>
     </Box>
   )
