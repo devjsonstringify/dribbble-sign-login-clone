a123a083dc7cff137a81a7625c11b375b550e26a
devjson <dev.json.stringify@gmail.com>
2021-11-04 15:53:26 +0800

    Update:  nextjs typescript

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,6 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res): void => {
+type Data = {
+  name: string
+}
+
+export default (req: NextApiRequest, res: NextApiResponse<Data>): void => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
