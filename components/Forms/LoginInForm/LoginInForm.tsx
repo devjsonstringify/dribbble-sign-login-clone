a105fa4a33b235f3612dc346321f3a44bef6ed97
devjson <dev.json.stringify@gmail.com>
2021-11-06 20:01:39 +0800

    Feat: integrate react-hooks-form on login page

--- components/Forms/LoginInForm/LoginInForm.tsx
+++ components/Forms/LoginInForm/LoginInForm.tsx
@@ -2,42 +2,70 @@ import * as React from 'react'
 import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
+import { useForm, Controller } from 'react-hook-form'
 import TextField from '../TextField'
 import Button from '@mui/material/Button'
 
+type FormValues = {
+  username: string
+  password: string
+}
+
 const LoginInForm = () => {
+  const { control, handleSubmit } = useForm<FormValues>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (values: unknown) => alert(JSON.stringify(values))
+
   return (
     <Box>
-      <TextField label="Username or Email Address" />
-      <Box sx={{ position: 'relative' }}>
-        <Link
-          href="/"
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          control={control}
+          name="username"
+          render={({ field }) => (
+            <TextField {...field} label="Username or Email Address" />
+          )}
+        />
+        <Box sx={{ position: 'relative' }}>
+          <Link
+            href="/"
+            sx={{
+              color: 'secondary.main',
+              textDecoration: 'underline',
+              position: 'absolute',
+              top: 0,
+              right: 0,
+            }}
+          >
+            Forgot password?
+          </Link>
+          <Controller
+            control={control}
+            name="password"
+            render={({ field }) => <TextField {...field} label="Password" />}
+          />
+        </Box>
+        <Button
+          type="submit"
+          size="large"
+          color="primary"
+          variant="contained"
           sx={{
-            color: 'secondary.main',
-            textDecoration: 'underline',
-            position: 'absolute',
-            top: 0,
-            right: 0,
+            borderRadius: '0.5rem',
+            textTransform: 'inherit',
+            minWidth: '12.5rem',
+            marginTop: '0.635rem',
+            width: { xs: '100%', md: 'auto' },
           }}
         >
-          Forgot password?
-        </Link>
-        <TextField label="Password" />
-      </Box>
-      <Button
-        size="large"
-        color="primary"
-        variant="contained"
-        sx={{
-          borderRadius: '0.5rem',
-          textTransform: 'inherit',
-          minWidth: '12.5rem',
-          marginTop: '0.635rem',
-          width: { xs: '100%', md: 'auto' },
-        }}
-      >
-        Sign In
-      </Button>
+          Sign In
+        </Button>
+      </form>
     </Box>
   )
 }
