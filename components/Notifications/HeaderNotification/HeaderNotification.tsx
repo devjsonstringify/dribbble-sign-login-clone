8116d1a602cdce9511d103dd94a9c2bf45156176
devjson <dev.json.stringify@gmail.com>
2021-11-23 19:45:31 +0800

    Add: headerNotification component

--- components/Notifications/HeaderNotification/HeaderNotification.tsx
+++ components/Notifications/HeaderNotification/HeaderNotification.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Fade from '@mui/material/Fade'
+import { IHasError } from '../../../containers/LayoutCtx'
 
-type notificationProps = {
-  isError: boolean
-  message: string
-}
-
-const HeaderNotification = (props: notificationProps): JSX.Element => {
-  return <div>header notification here...</div>
+const HeaderNotification = (props: IHasError): JSX.Element => {
+  const { isError, message } = props
+  return (
+    <Fade in={isError}>
+      <Box
+        sx={{
+          boxSizing: 'border-box',
+          backgroundColor: '#f55',
+          padding: '0.75rem',
+          textAlign: 'center',
+        }}
+      >
+        <Container maxWidth="lg">
+          <Typography sx={{ color: '#fff' }}>{message}</Typography>
+        </Container>
+      </Box>
+    </Fade>
+  )
 }
 
 export default HeaderNotification
